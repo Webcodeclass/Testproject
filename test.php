122834b001e471e2ebb71d2316e4a44ee2719ef2
khem <K_slot@outlook.co.th>
2017-07-03 16:56:57 +0700

    update php

---
+++ test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test php</title>
+
+<style>
+.clss_test{
+
+width: 450px;
+height: auto;
+margin: 0 0 0 auto;
+display: block;
+border: 1px  solid Red;
+
+
+}
+
+
+</style>
+
+  </head>
+
+  <body>
+
+<div class="class_test">
+
+<h2>dhjadsahjd</h2>
+
+</div>
+
+  </body>
+</html>
