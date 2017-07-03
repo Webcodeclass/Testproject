97f6c84a8099eeee8c73ab1e283ee514b9296e33
khem <K_slot@outlook.co.th>
2017-07-03 17:11:33 +0700

    khem

---
+++ test2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test php</title>
+
+<style>
+.clss_test{
+
+width: 450px;
+height: auto;
+margin: 0 0 0 auto;
+display: block;
+border: 1px  solid Red;
+
+
+}
+
+
+</style>
+
+  </head>
+
+  <body>
+
+<div class="class_test">
+
+<h2>dhjadsahjd</h2>
+<h2>dhjadsahjd TEST@2</h2>
+
+
+</div>
+
+
+
+
+  </body>
+</html>
